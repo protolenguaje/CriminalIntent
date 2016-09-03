b49cf42bec2490bde28243b9296b5f5b0a47f58f
protolenguaje <ivan.glez.aguilar@gmail.com>
2016-09-03 15:42:30 -0500

    done

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -5,25 +5,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fm = getSupportFragmentManager();
-
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, new CrimeFragment())
-                    .commit();
-
-            fragment = fm.findFragmentById(R.id.fragment_container);
-        }
-
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
