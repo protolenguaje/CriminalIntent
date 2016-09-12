17788a75172caa47e7ba113fb1dc6245bff614b3
protolenguaje <ivan.glez.aguilar@gmail.com>
2016-09-12 13:14:15 -0500

    dialogs completed

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -15,6 +16,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 import static android.text.format.DateFormat.*;
@@ -25,6 +27,8 @@ import static android.text.format.DateFormat.*;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -62,8 +66,13 @@ public class CrimeFragment extends Fragment {
         mTitleField.addTextChangedListener(new CrimeTextWatcher());
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(format("EEEE, dd MMMM, yyyy", mCrime.getDate()));
-        mDateButton.setEnabled(false);
+        updateDate();
+        mDateButton.setOnClickListener(view -> {
+            FragmentManager manager = getFragmentManager();
+            DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+            dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+            dialog.show(manager, DIALOG_DATE);
+        });
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
@@ -73,6 +82,28 @@ public class CrimeFragment extends Fragment {
                 });
 
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        mDateButton.setText(formatDate(mCrime.getDate()));
+    }
+
+    private CharSequence formatDate(Date date) {
+        return format("EEEE, dd MMMM, yyyy", date);
     }
 
     private final class CrimeTextWatcher implements TextWatcher {
